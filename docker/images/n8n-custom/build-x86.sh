916192b44503b160229b67f33733b5795e2d4181
Noam Loewenstern <noam.l@ocean.security>
2025-06-11 01:44:48 +0300

    updated custom docker

---
+++ docker/images/n8n-custom/build-x86.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -eo pipefail
+
+echo "Building patched n8n image"
+docker buildx build -t n8n-patch --platform linux/x86_64 -f docker/images/n8n-custom/Dockerfile --build-arg PLATFORM=linux/x86_64 .
--- docker/images/n8n-custom/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
-echo "Building patched n8n image"
-docker buildx build -t n8n-patch --platform linux/amd64 -f docker/images/n8n-custom/Dockerfile .
